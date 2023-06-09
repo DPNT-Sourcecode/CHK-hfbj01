c736a772f4e1b324d60a44b84c79100f1ab32886
sbx_user1051 <sbx_user1051@169.254.25.225>
2023-06-09 14:50:53 +0000

    Fri Jun 09 14:50:53 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -14,6 +14,7 @@ public class CheckoutSolution {
         }
 
         int cost = 0;
+        int freeB = 0;
         for(char ch: frequencyMap.keySet()) {
             if(ch == 'D') {
                 cost += frequencyMap.get(ch) * 15;
@@ -21,12 +22,17 @@ public class CheckoutSolution {
             else if(ch == 'C') {
                 cost += frequencyMap.get(ch) * 20;
             }
+            else if(ch == 'E') {
+                int frequencyE = frequencyMap.get(ch);
+                freeB = frequencyE / 2;
+                cost += frequencyE * 40;
+            }
             else if(ch == 'A') {
                 int discountCost = 130, frequencyA = frequencyMap.get(ch);
                 cost += ((frequencyA / 3) * discountCost) + (frequencyA % 3)*50;
             }
             else if(ch == 'B') {
-                int discountCost = 45, frequencyB = frequencyMap.get(ch);
+                int discountCost = 45, frequencyB = Math.max(0, frequencyMap.get(ch) - freeB);
                 cost += ((frequencyB / 2) * discountCost) + (frequencyB % 2)*30;
             }
             else return -1;
@@ -35,3 +41,4 @@ public class CheckoutSolution {
         return cost;
     }
 }
+
