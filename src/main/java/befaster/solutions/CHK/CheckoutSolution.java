1d4e8e7fea2ba104a9e1364eb1486055a3d84b1b
sbx_user1051 <sbx_user1051@169.254.25.225>
2023-06-09 15:16:32 +0000

    Fri Jun 09 15:16:32 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -9,10 +9,6 @@ public class CheckoutSolution {
 
     Map<Character, Integer> frequencyMap = new HashMap<>();
 
-    int applyDiscount(int frequency, int offerCount, int discountedPrice) {
-        int itemCount = frequency/
-    }
-
     int getCost() {
         int cost = 0;
 
@@ -31,12 +27,18 @@ public class CheckoutSolution {
             }
             else if(ch == 'A') {
                 int discountCost = 130, frequencyA = frequencyMap.get(ch);
-                int discountCost2 = 200;
+                int discount2 = 200;
 
-                int priceAfterDiscount1 =
+                int priceAfterDiscount1 = (frequencyA / 5) * discount2;
+                cost += priceAfterDiscount1;
+                frequencyA = frequencyA % 5;
 
-                5A for 200
-                cost += ((frequencyA / 3) * discountCost) + (frequencyA % 3)*50;
+                int priceAfterDiscount2 = (frequencyA / 3) * discountCost;
+                cost += priceAfterDiscount2;
+                frequencyA = frequencyA % 3;
+
+
+                return cost + (frequencyA*50);
             }
             else if(ch == 'B') {
                 int discountCost = 45, frequencyB = Math.max(0, frequencyMap.get(ch) - freeB);
@@ -56,3 +58,4 @@ public class CheckoutSolution {
         return getCost();
     }
 }
+
