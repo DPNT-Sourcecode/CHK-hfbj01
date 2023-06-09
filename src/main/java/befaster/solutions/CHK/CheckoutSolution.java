235b4627df808238ce6ec2f411f4464b5a9bae2e
sbx_user1051 <sbx_user1051@169.254.25.225>
2023-06-09 14:39:19 +0000

    Fri Jun 09 14:39:19 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -9,9 +9,30 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         Map<Character, Integer> frequencyMap = new HashMap<>();
         for(int i=0; i<skus.length(); i++){
-            char ch = skus.get(i);
+            char ch = skus.charAt(i);
             frequencyMap.put(ch, frequencyMap.getOrDefault(ch, 0) + 1);
         }
+
+        int cost = 0;
+        for(char ch: frequencyMap.keySet()) {
+            if(ch == 'D') {
+                cost += frequencyMap.get(ch) * 15;
+            }
+            else if(ch == 'C') {
+                cost += frequencyMap.get(ch) * 20;
+            }
+            else if(ch == 'A') {
+                int discountCost = 130, frequencyA = frequencyMap.get(ch);
+                cost += ((frequencyA / 3) * discountCost) + (frequencyA % 3)*50;
+            }
+            else if(ch == 'B') {
+                int discountCost = 45, frequencyB = frequencyMap.get(ch);
+                cost += ((frequencyB / 2) * discountCost) + (frequencyB % 2)*30;
+            }
+            else return -1;
+        }
+
+        return cost;
     }
 }
 
---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -0,0 +1,6 @@
+package befaster.solutions.CHK;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CheckoutSolutionTest {
+  
+}
